6c5a266ba9e955f14a9da8991fe6f5e9c3f62b6c
nopphasinara <nopphasin.arayasirikul@gmail.com>
2018-10-05 09:15:01 +0700

    Initial commit

---
+++ wp-content/themes/houzez-child/functions.php
@@ -0,0 +1,27 @@
+<?php
+
+// code will goes here
+
+// if( !function_exists('houzez_child_scripts') ) {
+//   function houzez_child_scripts()
+//   {
+//     wp_enqueue_script('houzez-child-scripts', get_stylesheet_directory_uri() . '/js/custom.js', array('jquery'), HOUZEZ_THEME_VERSION, true);
+//   }
+//
+//   add_action( 'wp_enqueue_scripts', 'houzez_child_scripts' );
+// }
+
+/* <ul class="social">
+  <li>
+    <a href="#" class="btn-facebook"><i class="fa fa-facebook-square"></i></a>
+  </li>
+  <li>
+    <a href="#" class="btn-twitter"><i class="fa fa-twitter-square"></i></a>
+  </li>
+  <li>
+    <a href="#" class="btn-google-plus"><i class="fa fa-google-plus-square"></i></a>
+  </li>
+  <li>
+    <a href="#" class="btn-linkedin"><i class="fa fa-linkedin-square"></i></a>
+  </li>
+</ul> */
