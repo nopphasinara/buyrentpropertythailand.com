c8307da54e7ebd6f064506f27049d10c5a416062
nopphasinara <nopphasin.arayasirikul@gmail.com>
2018-11-03 02:14:26 +0700

    Update source

--- demo/resources/views/welcome.blade.php
+++ demo/resources/views/welcome.blade.php
@@ -13,13 +13,90 @@
   </head>
   <body>
 
-    <main>
-      xxx
-    </main>
+    <!-- #header-section -->
+    <header id="header-section">
 
-    <footer>
-      xxx
+      <!-- .topbar -->
+      <div class="topbar">
+        <!-- .container -->
+        <div class="container">
+          <div class="d-flex flex-row justify-content-md-between">
+            <div class="col-auto">
+              .col-auto
+            </div>
+            <div class="col-auto">
+              .col-auto
+            </div>
+          </div>
+        </div>
+        <!-- / .container -->
+      </div>
+      <!-- / .topbar -->
+
+      <!-- .header-style-1 -->
+      <div class="header-style-1 header">
+        .header-style-1
+      </div>
+      <!-- / .header-style-1 -->
+
+      <!-- .header-style-2 -->
+      <div class="header-style-2 header">
+        .header-style-2
+      </div>
+      <!-- / .header-style-2 -->
+
+      <!-- .header-style-3 -->
+      <div class="header-style-3 header">
+        .header-style-3
+      </div>
+      <!-- / .header-style-3 -->
+
+      <!-- .header-style-4 -->
+      <div class="header-style-4 header">
+        .header-style-4
+      </div>
+      <!-- / .header-style-4 -->
+
+    </header>
+    <!-- / #header-section -->
+
+    <!-- #main-section -->
+    <main id="main-section">
+
+      <!-- #content-top-section -->
+      <div id="content-top-section">
+        <div class="h1">
+          .h1
+        </div>
+        <div class="display-1">
+          .display-1
+        </div>
+        <p class="lead">
+          .lead
+        </p>
+      </div>
+      <!-- / #content-top-section -->
+
+      <!-- #content-section -->
+      <div id="content-section">
+        content-section
+      </div>
+      <!-- / #content-section -->
+
+      <!-- #content-bottom-section -->
+      <div id="content-bottom-section">
+        content-bottom-section
+      </div>
+      <!-- / #content-bottom-section -->
+
+    </main>
+    <!-- / #main-section -->
+
+    <!-- #footer-section -->
+    <footer id="footer-section">
+      footer-section
     </footer>
+    <!-- / #footer-section -->
 
     <script src="{{ asset('js/app.js') }}" crossorigin="anonymous"></script>
     <script src="{{ asset('js/vendor/holderjs/holder.min.js') }}" crossorigin="anonymous"></script>
