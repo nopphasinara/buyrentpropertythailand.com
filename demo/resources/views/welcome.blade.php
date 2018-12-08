97b077f8d694d9dbec9eec31d94ab090ed8e2760
nopphasinara <nopphasin.arayasirikul@gmail.com>
2018-12-09 03:30:44 +0700

    Update source

--- demo/resources/views/welcome.blade.php
+++ demo/resources/views/welcome.blade.php
@@ -18,97 +18,146 @@
 
     <!-- #header-section -->
     <header id="header-section">
-
-      <!-- .topbar-style-1 -->
-      <div class="topbar-style-1 topbar bg-secondary">
-        <!-- .container-fluid -->
-        <div class="container-fluid px-0">
-          <div class="row d-flex flex-row justify-content-md-between align-items-center no-gutters">
-            <div class="left-panel panel col mr-auto">
-              <!-- .menu-topbar -->
-              <div class="menu-topbar btn-group" role="group" aria-label="Menu Topbar">
-                <a class="btn btn-outline-warning btn-hilight" href="#" role="button">Menu</a>
-                <a class="btn btn-secondary" href="#" role="button">Menu</a>
-                <a class="btn btn-secondary" href="#" role="button">Menu</a>
-              </div>
-              <!-- / .menu-topbar -->
-            </div>
-
-            <div class="right-panel panel col-auto">
-              <!-- .tools-topbar -->
-              <div class="tools-topbar btn-group" role="group" aria-label="Tools Topbar">
-                <a class="btn btn-primary" href="#" role="button">Sign In / Register <i class="fa fa-lg fa-user"></i></a>
-              </div>
-              <!-- / .tools-topbar -->
-            </div>
-          </div>
-        </div>
-        <!-- / .container-fluid -->
-      </div>
-      <!-- / .topbar-style-1 -->
-
-      <!-- .header-style-1 -->
-      <div class="header-style-1 header bg-dark text-light">
-        <!-- .container -->
-        <div class="container-fluid">
-          <div class="row d-flex flex-row justify-content-md-between align-items-center">
-            <div class="logo-header col-auto px-0 bg-white">
-              <a class="d-flex align-items-center px-4" href="{{ url('/') }}">
-                <img src="https://buyrentpropertythailand.com/wp-content/uploads/2018/09/wwwbrpt-logo.png" alt="Logo">
+      <!-- .header-mobile -->
+      <div class="header-mobile header d-md-none">
+        <div class="container">
+          <div class="row justify-content-between align-items-center py-3 bg-light">
+            <div class="col-auto">
+              <a class="btn btn-default-outline" data-toggle="collapse" data-target="#mobileMenu" aria-controls="mobileMenu" aria-expanded="false" aria-label="Toggle navigation" role="button">
+                <i class="fa fa-fw fa-lg fa-bars"></i>
               </a>
             </div>
-            <div class="col">
-              xxx
+            <div class="col-auto">
+              <a href="{{ url('/') }}">
+                <img src="{{ env('APP_LOGO') }}" alt="{{ env('APP_NAME') }}">
+              </a>
             </div>
             <div class="col-auto">
-              Search <i class="fa fa-lg fa-search"></i>
+              <a class="btn btn-default-outline" data-toggle="modal" data-target="#modalMobileUserLogin" href="#modalMobileUserLogin" role="button">
+                <i class="fa fa-fw fa-lg fa-user"></i>
+              </a>
             </div>
           </div>
         </div>
-        <!-- / .container -->
-      </div>
-      <!-- / .header-style-1 -->
 
+        <div class="collapse accordion bg-secondary" id="mobileMenu">
+          <a class="btn btn-block text-left" role="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne" href="#collapseOne">
+            Collapsible Group Item #1
+          </a>
+          <div class="collapse" id="collapseOne" aria-labelledby="headingOne" data-parent="#mobileMenu">
+            <ul class="navbar-nav mr-auto my-0">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+              </li>
+            </ul>
+          </div>
+          <a class="btn btn-block text-left" role="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo" href="#collapseTwo">
+            Collapsible Group Item #2
+          </a>
+          <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#mobileMenu">
+            <ul class="navbar-nav mr-auto my-0">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <div class="accordion" id="accordionExample2">
+                  <a class="btn btn-block text-left" role="button" data-toggle="collapse" data-target="#collapse4" aria-expanded="true" aria-controls="collapse4" href="#collapse4">
+                    Collapsible Group Item #4
+                  </a>
+                  <div id="collapse4" class="collapse bg-light" aria-labelledby="heading4" data-parent="#accordionExample2">
+                    <ul class="navbar-nav mr-auto my-0">
+                      <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+              </li>
+            </ul>
+          </div>
+          <a class="btn btn-block text-left" role="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree" href="#collapseThree">
+            Collapsible Group Item #3
+          </a>
+          <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#mobileMenu">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="modal fade" id="modalMobileUserLogin" tabindex="-1" role="dialog" aria-labelledby="modalMobileUserLoginTitle" aria-hidden="true">
+          <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="modalMobileUserLoginTitle">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                ...
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- / .header-mobile -->
+
+      <div id="homepageSlide" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#homepageSlide" data-slide-to="0" class="active"></li>
+          <li data-target="#homepageSlide" data-slide-to="1"></li>
+          <li data-target="#homepageSlide" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img class="d-block w-100" data-src="holder.js/800x400?auto=yes&bg=777&fg=555&text=First slide" src="..." alt="slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" data-src="holder.js/800x400?auto=yes&bg=666&fg=444&text=Second slide" src="..." alt="slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" data-src="holder.js/800x400?auto=yes&bg=555&fg=333&text=Third slide" src="..." alt="slide">
+          </div>
+        </div>
+        <a class="carousel-control carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
     </header>
     <!-- / #header-section -->
-
-    <!-- #main-section -->
-    <main id="main-section">
-
-      <!-- #content-top-section -->
-      <div id="content-top-section">
-        <div class="h1">
-          .h1
-        </div>
-        <div class="display-1">
-          .display-1
-        </div>
-        <p class="lead">
-          .lead
-        </p>
-      </div>
-      <!-- / #content-top-section -->
-
-      <!-- #content-section -->
-      <div id="content-section">
-        content-section
-      </div>
-      <!-- / #content-section -->
-
-      <!-- #content-bottom-section -->
-      <div id="content-bottom-section">
-        content-bottom-section
-      </div>
-      <!-- / #content-bottom-section -->
-
-    </main>
-    <!-- / #main-section -->
-
-    <!-- #footer-section -->
-    <footer id="footer-section">
-      footer-section
-    </footer>
-    <!-- / #footer-section -->
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" crossorigin="anonymous"></script>
     <script src="{{ asset('js/app.js') }}" crossorigin="anonymous"></script>
