31b79ce34371bb758e2f3ef1073b3c7d529912e1
nopphasinara <nopphasin.arayasirikul@gmail.com>
2018-12-12 01:53:27 +0700

    Update composer

--- index.php
+++ index.php
@@ -13,5 +13,24 @@
  */
 define('WP_USE_THEMES', true);
 
+
+// require_once $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
+//
+// session_start();
+//
+// $client = new Google_Client();
+// $client->setAuthConfig($_SERVER['DOCUMENT_ROOT'] . '/client_secret.json');
+// $client->addScope(Google_Service_Drive::DRIVE_METADATA_READONLY);
+//
+// if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
+//   $client->setAccessToken($_SESSION['access_token']);
+//   $drive = new Google_Service_Drive($client);
+//   $files = $drive->files->listFiles(array())->getItems();
+//   echo json_encode($files);
+// } else {
+//   $redirect_uri = 'https://' . $_SERVER['HTTP_HOST'] . '/oauth2callback.php';
+//   header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
+// }
+
 /** Loads the WordPress Environment and Template */
 require( dirname( __FILE__ ) . '/wp-blog-header.php' );
