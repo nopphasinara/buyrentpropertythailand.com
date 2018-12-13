b507e4a4069586f05828df54c513c44151715e39
nopphasinara <nopphasin.arayasirikul@gmail.com>
2018-12-13 16:48:53 +0700

    Update source

--- resources/views/home-1.blade.php
+++ resources/views/home-1.blade.php
@@ -5,13 +5,13 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
   <meta name="keywords" content="">
-  <meta name="author" content="{{ env('APP_AUTHOR') }}">
-  <title>Blog Template for Bootstrap</title>
+  <meta name="author" content="{{ env('APP_AUTHOR', 'Goldfish Creative Thailand') }}">
+  <title>Homepage</title>
 
-  <link href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous" rel="stylesheet">
+  <link href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-
 <body>
 
   <!-- #headerSection -->
