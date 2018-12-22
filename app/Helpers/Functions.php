a9f73e6c16c1ec9aa246e02cbcb59a1cc5cbda0f
nopphasinara <nopphasin.arayasirikul@gmail.com>
2018-12-23 00:47:59 +0700

    Install Backpack package

---
+++ app/Helpers/Functions.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Common Helper Functions
+|--------------------------------------------------------------------------
+|
+| Here you create all of the helper functions that your application supports.
+|
+*/
---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        require __DIR__ . '/../Helpers/Functions.php';
+
+        Schema::defaultStringLength(190);
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
