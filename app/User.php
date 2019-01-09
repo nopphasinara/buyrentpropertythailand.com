0a90d2b822f06b5eb44e5109ceb914b2e61098a2
nopphasinara <nopphasin.arayasirikul@gmail.com>
2019-01-09 10:21:46 +0700

    Update source

--- app/User.php
+++ app/User.php
@@ -2,40 +2,68 @@
 
 namespace App;
 
+use Cviebrock\EloquentSluggable\Sluggable;
+use Spatie\MediaLibrary\HasMedia\HasMedia;
+use Spatie\MediaLibrary\HasMedia\HasMediaTrait;
+use Intervention\Image\ImageManager;
+
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements HasMedia
 {
-    use Notifiable;
+  use Notifiable;
+  use Sluggable;
+  use HasMediaTrait;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password', 'user_level', 'is_active',
+  /**
+  * The attributes that are mass assignable.
+  *
+  * @var array
+  */
+  protected $fillable = [
+    'name', 'email', 'password', 'user_level', 'is_active',
+  ];
+
+  /**
+  * The attributes that should be hidden for arrays.
+  *
+  * @var array
+  */
+  protected $hidden = [
+    'password', 'remember_token',
+  ];
+
+  protected $appends = [
+    // 'name',
+  ];
+
+  // protected static $sports = [
+  //    'soccer' => 'sport:1',
+  //    'tennis' => 'sport:2',
+  //    'basketball' => 'sport:3',
+  //    ...
+  // ];
+
+  /**
+  * Return the sluggable configuration array for this model.
+  *
+  * @return array
+  */
+  /*
+  public function sluggable()
+  {
+    return [
+      'slug' => [
+        'source' => 'title',
+        'separator' => '-',
+        'method' => function ($string, $separator) {
+          return strtolower(preg_replace('/[^a-z]+/i', $separator, $string));
+        },
+        'onUpdate' => true,
+      ],
     ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
-
-    protected $appends = [
-      // 'name',
-   ];
-
-   // protected static $sports = [
-   //    'soccer' => 'sport:1',
-   //    'tennis' => 'sport:2',
-   //    'basketball' => 'sport:3',
-   //    ...
-   // ];
+  }
+  */
 }
--- config/app.php
+++ config/app.php
@@ -176,6 +176,8 @@ return [
         // App\Providers\HorizonServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Intervention\Image\ImageServiceProvider::class,
+
     ],
 
     /*
@@ -224,6 +226,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'Image' => Intervention\Image\Facades\Image::class,
 
     ],
 
